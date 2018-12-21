be5f773e1b84a204aa605440418f65ad5d133eef
ShawnArnold1 <shawnthearnold@gmail.com>
2018-12-21 17:00:50 -0600

    Added button and editable table data

--- src/movieTools.js
+++ src/movieTools.js
@@ -1,14 +1,15 @@
 
 const buildMovieHtml = (movies) => {
-    let html = `<table><form action="">`;
+    let html = `<h3>Click a movie to edit</h3><table>`;
     let idMovieNumber = movies.length;
-    movies.forEach((movie) => {
-        html += `<tr><td>${movie.title}</td><td>Rating: ${movie.rating}</td><td><button class="btn" type="button">Edit Movie</button></td></tr>`
+    movies.forEach((movie, id) => {
+        html += `<tr><td class="movies${id}" contenteditable="true">${movie.title}</td><td contenteditable="true">Rating: ${movie.rating}</td></tr>`
     });
+
     console.log(idMovieNumber);
-    return html += `</table></form>`;
+    return html += `<tr><td><button class="btn" type="button">Submit Changes</button></td></tr></table>`;
 };
 
-
+// document.getElementsByClassName('movies0')[0].innerText
 
 export default {buildMovieHtml};
